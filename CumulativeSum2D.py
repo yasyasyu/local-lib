be6b994f439e674102c170459e60f2a8645c21d1
yasyasyu <yasyu.hitoha@gmail.com>
2023-12-03 09:20:57 +0900

    fix cumulativesum2d

--- CumulativeSum2D.py
+++ CumulativeSum2D.py
@@ -2,28 +2,23 @@ class CumulativeSum2D:
     def __init__(self, H: int, W: int) -> None:
         self.H = H
         self.W = W
-        self.H1 = H + 1
-        self.W1 = W + 1
-        self.arr = [0] * (self.W * self.H)
-        self.carr = [0] * (self.H1 * self.W1)
+        self.grid = [[0] * (W + 1) for _ in range(H + 1)]
 
     def add(self, i: int, j: int, v: int = 1) -> None:
-        self.arr[i * self.W + j] += v
+        self.grid[(i + 1) * self.W + (j + 1)] += v
 
     def build(self) -> None:
-        for i in range(self.H):
+        for i in range(self.H + 1):
             for j in range(self.W):
-                self.carr[(i + 1) * self.W1 + (j + 1)] = (
-                    self.carr[(i) * self.W1 + (j + 1)]
-                    + self.carr[(i + 1) * self.W1 + (j)]
-                    - self.carr[(i) * self.W1 + (j)]
-                    + self.arr[(i) * self.W + (j)]
-                )
+                self.grid[i * (self.W + 1) + j + 1] += self.grid[i * (self.W + 1) + j]
+        for j in range(self.W + 1):
+            for i in range(self.H):
+                self.grid[(i + 1) * (self.W + 1) + j] += self.grid[i * (self.W + 1) + j]
 
     def get(self, i: int, j: int, ii: int, jj: int) -> int:
         return (
-            self.carr[ii * self.W1 + jj]
-            - self.carr[ii * self.W1 + j]
-            - self.carr[i * self.W1 + jj]
-            + self.carr[i * self.W1 + j]
+            self.grid[ii * (self.W + 1) + jj]
+            - self.grid[(i - 1) * (self.W + 1) + jj]
+            - self.grid[(ii) * (self.W + 1) + (j - 1)]
+            + self.grid[(i - 1) * (self.W + 1) + (j - 1)]
         )
