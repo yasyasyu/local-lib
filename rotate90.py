981e00a05812369d60015871bfe41d07be74d862
yasyasyu <yasyu.hitoha@gmail.com>
2024-06-03 01:37:10 +0900

    add transpose

--- rotate90.py
+++ rotate90.py
@@ -2,3 +2,7 @@ def rotate90(grid):
     for _ in range(4):
         grid = [list(g) for g in zip(*grid[::-1])]
         yield grid
+
+
+def transpose(grid):
+    return [list(g) for g in zip(*grid)]
