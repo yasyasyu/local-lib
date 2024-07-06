301b1f30b6ca1bf93e1f104e5b1393291291aba0
yasyasyu <yasyu.hitoha@gmail.com>
2024-07-06 20:27:14 +0900

    update CumulativeSum2D

--- CumulativeSum2D.py
+++ CumulativeSum2D.py
@@ -1,24 +1,29 @@
+from typing import Any, List
+
+
 class CumulativeSum2D:
-    def __init__(self, H: int, W: int) -> None:
-        self.H = H
-        self.W = W
-        self.grid = [[0] * (W + 1) for _ in range(H + 1)]
 
-    def add(self, i: int, j: int, v: int = 1) -> None:
-        self.grid[(i + 1) * self.W + (j + 1)] += v
+    def __init__(self, array: List[List[Any]]) -> None:
+        self.height: int = len(array)
+        self.width: int = len(array[0])
+        self.summed_array: List[List[Any]] = [
+            [0] * (self.width + 1) for _ in range(self.height + 1)
+        ]
 
-    def build(self) -> None:
-        for i in range(self.H + 1):
-            for j in range(self.W):
-                self.grid[i * (self.W + 1) + j + 1] += self.grid[i * (self.W + 1) + j]
-        for j in range(self.W + 1):
-            for i in range(self.H):
-                self.grid[(i + 1) * (self.W + 1) + j] += self.grid[i * (self.W + 1) + j]
+        for i in range(self.height):
+            for j in range(self.width):
+                self.summed_array[i + 1][j + 1] = (
+                    self.summed_array[i + 1][j]
+                    + self.summed_array[i][j + 1]
+                    - self.summed_array[i][j]
+                    + array[i][j]
+                )
 
-    def get(self, i: int, j: int, ii: int, jj: int) -> int:
+    def query(self, i: int, j: int, ii: int, jj: int) -> Any:
+        # i, j, ii, jj: 0-indexed
         return (
-            self.grid[ii * (self.W + 1) + jj]
-            - self.grid[(i - 1) * (self.W + 1) + jj]
-            - self.grid[(ii) * (self.W + 1) + (j - 1)]
-            + self.grid[(i - 1) * (self.W + 1) + (j - 1)]
+            self.summed_array[ii][jj]
+            - self.summed_array[ii][j]
+            - self.summed_array[i][jj]
+            + self.summed_array[i][j]
         )
