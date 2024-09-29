302b9e7d73a779b9630ddf4957570b9a34a5b407
yasyasyu <yasyu.hitoha@gmail.com>
2024-09-30 02:01:31 +0900

    deciding binary_search inner judge

--- deciding_binary_search.py
+++ deciding_binary_search.py
@@ -1,14 +1,14 @@
-def is_ok(mid):
-    if mid > 0:
-        return True
-    else:
-        return False
-
-
 def binary_search(ng, ok):
     """
     (ng : ok)
     """
+
+    def is_ok(mid):
+        if mid > 0:
+            return True
+        else:
+            return False
+
     while abs(ok - ng) > 1:
         mid = (ok + ng) // 2
         if is_ok(mid):
